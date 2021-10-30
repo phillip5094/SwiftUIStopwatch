64a3f5ea4e6d8cda123912b4a059c1fe8dc24b2b
정필립/게임플랫폼클라팀/NE <philip.chung@nhn.com>
2021-10-31 00:56:29 +0900

    Initial Commit

---
+++ Stopwatch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stopwatch
+//
+//  Created by NHN on 2021/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by NHN on 2021/10/31.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
